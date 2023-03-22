40dd79539b3d10ad18f3c0a4b6f44b3377948bd5
ismaelacosta <ismael10barca@hotmail.com>
2023-03-22 09:15:25 -0700

    clase 4 - avance

---
+++ Shopping/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shopping.Data.Entities;
+
+namespace Shopping.Data;
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) :base(options)
+    {
+    }
+
+    public DbSet<Country> Countries { get; set; }
+}
