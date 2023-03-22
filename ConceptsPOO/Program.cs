50c4305bca64e9072a34a44dc897aed321f8b0e9
ismaelacosta <ismael10barca@hotmail.com>
2023-03-21 20:56:44 -0700

    Primera clase - Clase Date

---
+++ ConceptsPOO/Program.cs
@@ -0,0 +1,19 @@
+﻿using ConceptsPOO;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+try
+{
+    Console.WriteLine(new Date(2023, 3, 21));
+    Console.WriteLine(new Date(1974, 9, 23));
+    Console.WriteLine(new Date(2024, 2, 29));
+}
+catch (Exception error)
+{
+    Console.WriteLine(error.Message);
+}
+
+
+
+
