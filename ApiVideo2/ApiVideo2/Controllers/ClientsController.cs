9afe99c4f698c1f7ae36862fafd4b57693b85f14
Roberto Ismael Acosta Lopez <iacosta@myrobal.com>
2023-03-31 07:26:40 -0600

    Video 2 - API

---
+++ ApiVideo2/ApiVideo2/Controllers/ClientsController.cs
@@ -0,0 +1,85 @@
+﻿using ApiVideo2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiVideo2.Controllers
+{
+
+    [ApiController]
+    [Route("client")]
+    public class ClientsController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("/listall")]
+        public dynamic ListClients()
+        {
+
+             List<Client> clients = new List<Client>
+             {
+                 new Client { 
+                     Id = 1,
+                     Name = "Ismael",
+                     Age = 22,
+                     Email = "ismael@yopmail"
+                 },
+                 new Client {
+                     Id = 2,
+                     Name = "Rafa",
+                     Age = 17,
+                     Email = "rafa@yopmail"
+                 },
+             };
+
+
+            return clients;
+        }
+
+        [HttpGet]
+        [Route("/id")]
+        public dynamic ViewClient(int id)
+        {
+          Client client =   new Client
+            {
+                Id = 1,
+                Name = "Ismael",
+                Age = 22,
+                Email = "ismael@yopmail"
+            };
+
+            return client;
+        }
+
+
+        [HttpPost]
+        [Route("/save")]
+        public dynamic SaveClient( Client client)
+        {
+            client.Id = 3;
+            return new
+            {
+                success = true,
+                message = " cliente registrado",
+                result = client
+            };
+        }
+
+        [HttpDelete]
+        [Route("/delete")]
+        public dynamic DeleteClient(int id)
+        {
+            string token = Request.Headers.Where(x => x.Key == "Authorization").FirstOrDefault().Value;   // todo viene en esta variable
+
+            if(token == "ismael")
+            {
+                Console.WriteLine("Acceso Correcto");
+            }
+
+            return new
+            {
+                success = true,
+                message = " cliente eliminado",
+                result = id
+            };
+        }
+    }
+}
---
+++ ApiVideo2/ApiVideo2/Models/Client.cs
@@ -0,0 +1,14 @@
+﻿namespace ApiVideo2.Models
+{
+    public class Client
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Email { get; set; }
+
+    }
+}
